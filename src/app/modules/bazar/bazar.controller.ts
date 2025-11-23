3589fd8ce9db43bd8c656cdb864920e5fa546d02
samio11 <samiohasan6@gmail.com>
2025-11-24 00:51:30 +0600

    Bazar Tested

--- src/app/modules/bazar/bazar.controller.ts
+++ src/app/modules/bazar/bazar.controller.ts
@@ -2,16 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { bazarServices } from "./bazar.services";
 import { sendResponse } from "../../utils/sendResponse";
+import { JwtPayload } from "jsonwebtoken";
 
 const createBazar = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { messId } = req?.params;
-    const { id } = req?.user;
+    const { id } = req?.user as JwtPayload;
     const payload = {
       ...JSON.parse(req?.body?.data),
       proof: req?.file?.path,
+      mess: messId,
       addedBy: id,
     };
+
     const result = await bazarServices.createBazar(payload, messId, id);
     sendResponse(res, {
       success: true,
--- src/app/modules/bazar/bazar.routes.ts
+++ src/app/modules/bazar/bazar.routes.ts
@@ -2,9 +2,15 @@ import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { ERole } from "../user/user.interface";
 import { bazarController } from "./bazar.controller";
+import { multerUpload } from "../../config/multer.config";
 
 const router = Router();
 
-router.post("/create", checkAuth([ERole.member]), bazarController.createBazar);
+router.post(
+  "/create/:messId",
+  checkAuth([ERole.member]),
+  multerUpload.single("file"),
+  bazarController.createBazar
+);
 
 export const bazarRoutes = router;
