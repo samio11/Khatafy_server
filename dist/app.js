734d1c6bf79b0ef08de56457d5d0e47820bb2b94
samio11 <samiohasan6@gmail.com>
2025-12-01 09:35:08 +0600

    vercel file added

--- dist/app.js
+++ dist/app.js
@@ -19,7 +19,7 @@ const notFound_1 = require("./app/middlewares/notFound");
 const routes_1 = require("./app/routes");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "*", credentials: true }));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/api/v1", routes_1.rootRoute);
